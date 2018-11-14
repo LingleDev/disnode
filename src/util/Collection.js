752852902e01d2cf8dc451fbd8a8d8ed0cc4667c
FHGDev <gideon.lingle1@gmail.com>
2018-11-14 10:23:27 -0500

    Create Collection.js

---
+++ src/util/Collection.js
@@ -0,0 +1,13 @@
+module.exports = class Collection extends Map {
+    set(key, value) {
+        super.set(key, value)
+    }
+
+    get(key) {
+        super.get(key)
+    }
+
+    delete(key) {
+        super.delete(key)
+    }
+}
