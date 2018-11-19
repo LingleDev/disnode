f9a91f5e8e2b82161e067409a284f516c7af3c85
FHGDev <gideon.lingle1@gmail.com>
2018-11-19 15:38:14 -0500

    Update Client.js

--- src/Client/Client.js
+++ src/Client/Client.js
@@ -1,6 +1,6 @@
 const EventEmitter = require('events')
 const Collection = require('../util/Collection')
-const websocket = require("../websocket.js")
+const websocket = require("../webSocket.js")
 
 module.exports = class Client extends EventEmitter {
   constructor(token, options) {
