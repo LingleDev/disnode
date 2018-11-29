17244044f7f28423a20fa929ae0b7eefa3088d2a
FHGDev <gideon.lingle1@gmail.com>
2018-11-29 12:40:06 -0600

    Update Guild.js

--- src/models/Guild.js
+++ src/models/Guild.js
@@ -1,7 +1,18 @@
+const Client = require('../Client/Client')
+
 module.exports = class Guild {
   constructor(obj) {
     for (const [key, value] of Object.entries(obj)) {
       this[key] = value;
     }
+  }
+  
+  ban(userid, reason) {
+    const snekfetch = require('snekfetch')
+    snekfetch.put(`https://discordapp.com/api/guilds/${this.id}/bans/${userid}`)
+    .set("Authorization", `Bot ${new Client().token}`)
+    .set("Content-Type", "application/json")
+    .send({ "delete-message-days": 7 })
+    .send({ "reason": reason || "no reason provided." })
   }
 }
